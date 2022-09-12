5f729fb39ca508ad74ef6877d44752b0be7a1eba
sfjbssept <sfjbssept@gmail.com>
2022-09-12 04:18:32 +0000

    entity has been added to product management

--- ProductManagementSystem/src/main/java/com/product/Entity/Product.java
+++ ProductManagementSystem/src/main/java/com/product/Entity/Product.java
@@ -1,5 +1,71 @@
 package com.product.Entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String productName;
+	private String productBrand;
+	private String productCatagory;
+	private String productPrice;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductBrand() {
+		return productBrand;
+	}
+
+	public void setProductBrand(String productBrand) {
+		this.productBrand = productBrand;
+	}
+
+	public String getProductCatagory() {
+		return productCatagory;
+	}
+
+	public void setProductCatagory(String productCatagory) {
+		this.productCatagory = productCatagory;
+	}
+
+	public String getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(String productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public Product(Integer id, String productName, String productBrand, String productCatagory, String productPrice) {
+		super();
+		this.id = id;
+		this.productName = productName;
+		this.productBrand = productBrand;
+		this.productCatagory = productCatagory;
+		this.productPrice = productPrice;
+	}
+
+	public Product() {
+		super();
+	}
 
 }
